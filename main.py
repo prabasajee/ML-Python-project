43605e84e72622993f5d2b4c7757229029531d98
prabasajee <prabaharansajeevan23@gmail.com>
2025-07-05 22:15:52 +0530

    change

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 # Example main.py for testing RPS player
 from RPS import player
-import random
-
-def random_bot(prev_play, _=[]):
-    return random.choice(["R", "P", "S"])
-
-def play(player1, player2, num_games=100, verbose=False):
-    p1_history = []
-    p2_history = []
-    p1_prev = ""
-    p2_prev = ""
-    p1_score = 0
-    p2_score = 0
-    for i in range(num_games):
-        move1 = player1(p2_prev, p2_history)
-        move2 = player2(p1_prev, p1_history)
-        p1_history.append(move1)
-        p2_history.append(move2)
-        p1_prev = move1
-        p2_prev = move2
-        if verbose:
-            print(f"Game {i+1}: Player1={move1}, Player2={move2}")
-        if move1 == move2:
-            continue
-        elif (move1 == "R" and move2 == "S") or (move1 == "P" and move2 == "R") or (move1 == "S" and move2 == "P"):
-            p1_score += 1
-        else:
-            p2_score += 1
-    print(f"Final Score: Player1={p1_score}, Player2={p2_score}")
+from RPS_game import play, quincy, abbey, kris, mrugesh
 
 if __name__ == "__main__":
-    play(player, random_bot, 100, verbose=True)
+    print("Testing against quincy:")
+    play(player, quincy, 1000, verbose=False)
+    print("Testing against abbey:")
+    play(player, abbey, 1000, verbose=False)
+    print("Testing against kris:")
+    play(player, kris, 1000, verbose=False)
+    print("Testing against mrugesh:")
+    play(player, mrugesh, 1000, verbose=False)
+
+    # Uncomment the next lines if you want to run the unit tests (if available)
+    # import test_module
+    # test_module.test_player()
